e1217ad5729874c41486c178754b7cc791d7c95a
Daniel Coelho <coelhodaniel06087@gmail.com>
2024-08-21 12:31:29 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,7 @@ public class Program {
 		Department d1 = new Department("Books",1);
 		Seller s1 = new Seller("Davi", 12,"davi@gmail.com", new Date(), 2900.0, d1);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
 		System.out.println(s1);
