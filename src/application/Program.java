90c0fb51eabed414e4589c4033e8cb803afa30c4
Daniel Coelho <coelhodaniel06087@gmail.com>
2024-08-21 12:03:18 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,8 +11,11 @@ public class Program {
 		
 		
 		Department d1 = new Department("Books",1);
-		System.out.print(d1);
+		Seller s1 = new Seller("Davi", 12,"davi@gmail.com", new Date(), 2900.0, d1);
 		
+		
+		
+		System.out.println(s1);
 	}
 
 }
